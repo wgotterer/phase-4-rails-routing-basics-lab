6d5d7d0a7e86cf4868d550070a65993b51691f6b
William Gotterer <william.gotterer@gmail.com>
2021-11-21 16:51:36 -0500

    commit tree bda3d6fd013e746b83035569e4f52a51ad1415e4

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -2,6 +2,14 @@ class StudentsController < ApplicationController
     def index
         students = Student.all
         render json: students
-    
+    end
+
+    def grades
+        # grades = Grade.all
+        # render json: grades
+        grades = Student.all.order(grade: :desc)
+
+        render json: grades
+
     end
 end
